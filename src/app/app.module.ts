998135500e7a83ec0912ce122e8bf509044d1e3e
basil4ever <basil4ever@mail.ru>
2023-05-02 14:10:59 +0300

    Create angular project Create employee class Create employee-list ----------------------------- Сreate-employee component update-employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import {FormsModule} from "@angular/forms";
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Employee} from "../employee";
+import {EmployeeService} from "../employee.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-employee-list',
@@ -10,9 +12,20 @@ export class EmployeeListComponent implements OnInit {
 
   employees: Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+              private router: Router) {}
 
   ngOnInit(): void {
+    this.getEmployees();
   }
 
+  private getEmployees() {
+    this.employeeService.getEmployeesList().subscribe(data =>{
+      this.employees = data;
+    });
+  }
+
+  updateEmployee(id: number) {
+    this.router.navigate(['update-emploee',id])
+  }
 }
